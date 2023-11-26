2fdac9fb314d4f764c111a0b2d0f794dfa8345e4
Sadaatva <150649176+Sadaatva@users.noreply.github.com>
2023-11-26 19:26:51 +0400

    Update sha1.cpp
    Added two parts

--- SHA1/sha1.cpp
+++ SHA1/sha1.cpp
@@ -26,3 +26,14 @@ std::string SHA1(const std::string& message){
     }
     
     imessage += std::string(reinterpret_cast<char*>(&message_length), sizeof(message_length));
+
+    for (size_t i = 0; i < imessage.length(); i += 64){
+        const unsigned int* part = reinterpret_cast<const unsigned int*>(&imessage[i]);
+
+        for (t = 0; t < 16; t++){
+            W[t] = part[t];
+        }
+        for (t = 16; t < 80; t++){
+            W[t] = left_rol(W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16], 1);
+        }
+    }
