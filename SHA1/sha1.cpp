3187a0b6610f5a71c914547041dd65802c7647ae
Sadaatva <150649176+Sadaatva@users.noreply.github.com>
2023-11-26 19:19:20 +0400

    Update sha1.cpp
    Initialize variables

--- SHA1/sha1.cpp
+++ SHA1/sha1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+unsigned int left_rol(unsigned int value, size_t bits){
+    return (value << bits) | (value >> (32 - bits));
+}
+
+std::string SHA1(const std::string& message){
+	
+    unsigned int H0 = 0x67452301;
+    unsigned int H1 = 0xEFCDAB89;
+    unsigned int H2 = 0x98BADCFE;
+    unsigned int H3 = 0x10325476;
+    unsigned int H4 = 0xC3D2E1F0;
+
+    unsigned int K;
+    unsigned int t;
+    unsigned int f;
+    unsigned int W[80];
 
