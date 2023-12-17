629cad43ee532805cfe8916bfe0d55742da3c0f3
Sadaatva <150649176+Sadaatva@users.noreply.github.com>
2023-12-17 10:17:47 +0400

    Update sha1.cpp
    Corrected version. Using "Vector"

--- SHA1/sha1.cpp
+++ SHA1/sha1.cpp
@@ -1,59 +1,64 @@
 #include <iostream>
+#include <cstdint>
+#include <vector>
 
 unsigned int left_rol(unsigned int value, size_t bits){
     return (value << bits) | (value >> (32 - bits));
 }
 
-std::string SHA1(const std::string& message){
-	
-    unsigned int H0 = 0x67452301;
-    unsigned int H1 = 0xEFCDAB89;
-    unsigned int H2 = 0x98BADCFE;
-    unsigned int H3 = 0x10325476;
-    unsigned int H4 = 0xC3D2E1F0;
+std::string SHA1(const std::string& vec){
+    std::vector<uint8_t> message(vec.begin(), vec.end());
+    
+    uint64_t imessage = message.size() * 8;
 
+    message.push_back(0x80);
+
+    while ((message.size() * 8) % 512 != 448){
+        message.push_back(0x00);
+    }
+    
+    for (int i = 7; i >= 0; --i){
+        message.push_back((imessage >> (i * 8)) & 0xFF);
+    }
+
+    std::vector<uint32_t> H = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0};
+    std::vector<uint32_t> W(80);
+    std::vector<uint32_t> M(16); 
+    
     unsigned int K;
     unsigned int t;
     unsigned int f;
-    unsigned int W[80];
-
-    unsigned int message_length = message.length() * 8;
     
-    std::string imessage = message + static_cast<char>(0x80);
-    
-    while((imessage.length() * 8) % 512 != 448){
-        imessage += static_cast<char>(0x00);
+    for (uint64_t n = 0; n*64 < message.size(); n++){ 
+		
+    for (int i = 0; i < 16; i++) {
+        M[i] = (message[n * 64 + 4 * i] << 24) | (message[n * 64 + 4 * i + 1] << 16) | (message[n * 64 + 4 * i + 2] << 8) | message[n * 64 + 4 * i + 3];
     }
-    
-    imessage += std::string(reinterpret_cast<char*>(&message_length), sizeof(message_length));
+ 
+    for (int i = 0; i < 16; i++){  
+        W[i] = M[i];  
+    } 
+    for (t = 16; t < 80; t++){
+	    W[t] = left_rol(W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16], 1);
+    }
 
-    for (size_t i = 0; i < imessage.length(); i += 64){
-        const unsigned int* part = reinterpret_cast<const unsigned int*>(&imessage[i]);
-
-        for (t = 0; t < 16; t++){
-            W[t] = part[t];
-        }
-        for (t = 16; t < 80; t++){
-            W[t] = left_rol(W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16], 1);
-        }
-	    
-	unsigned int a = H0;
-        unsigned int b = H1;
-        unsigned int c = H2;
-        unsigned int d = H3;
-        unsigned int e = H4;
+        unsigned int a = H[0];
+        unsigned int b = H[1];
+        unsigned int c = H[2];
+        unsigned int d = H[3];
+        unsigned int e = H[4];
 
         for (t = 0; t < 80; t++){
-            if (t == 0 || t < 20){
+            if (t < 20) {
                 K = 0x5a827999;
-                f = (b & c) | ((~b) & d);
+                f = (b & c) | ((~b) & d); 
             } else if (t < 40){
                 K = 0x6ed9eba1;
-                f = b ^ c ^ d;
+                f = b ^ c ^ d;   
             } else if (t < 60){
                 K = 0x8f1bbcdc;
-                f = (b & c) | (b & d) | (c & d);
-            } else {
+                f = (b & c) | (b & d) | (c & d); 
+            } else{
                 K = 0xca62c1d6;
                 f = b ^ c ^ d;
             }
@@ -65,21 +70,21 @@ std::string SHA1(const std::string& message){
             b = a;
             a = temp;
         }
-        H0 += a;
-        H1 += b;
-        H2 += c;
-        H3 += d;
-        H4 += e;
+        H[0] += a;
+        H[1] += b;
+        H[2] += c;
+        H[3] += d;
+        H[4] += e;
     }
 
     char all[41];  
-    std::sprintf(all, "%08x%08x%08x%08x%08x", H0, H1, H2, H3, H4);
+    std::sprintf(all, "%08x%08x%08x%08x%08x", H[0], H[1], H[2], H[3], H[4]);
 	
     return std::string(all);
 }
 
-int main() {
-    std::string message = "abcd";
+int main(){
+    std::string message = "'' '";
     std::string Hash = SHA1(message);
 
     std::cout << "SHA-1 hash: " << Hash << "\n";
